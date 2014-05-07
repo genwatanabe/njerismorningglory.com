d7891ce48f9ede7f596a96a0a19c7d41dda96643
Gen Watanabe <gen.watanabe.ca@gmail.com>
2014-05-06 22:42:46 -0700

    Landing page - fade in Njeri's phrase.

--- site/app/landing/script-src.js
+++ site/app/landing/script-src.js
@@ -4,23 +4,19 @@ var landingModule = angular.module('app.landing', [
   'ui.bootstrap'
 ]);
 
-landingModule.controller('LandingCtrl', ['$scope', '$routeParams', 'GlobalData', function($scope, $routeParams, GlobalData) {
-  $scope.gData = GlobalData;
-
-  $scope.model = {
-    message: ""
-  };
-}]);
-
 landingModule.factory('LandingCarouselService', ['$resource', function($resource) {
   return $resource('app/landing/data/carousel-ad.json'+'?_=' + Math.random());
 }]);
 
-landingModule.controller('LandingCarouselCtrl', ['$scope', 'LandingCarouselService', function($scope, LandingCarouselService) {
+landingModule.controller('LandingCtrl', ['$scope', '$routeParams', 'GlobalData', 'LandingCarouselService', function($scope, $routeParams, GlobalData, LandingCarouselService) {
+  $scope.gData = GlobalData;
   $scope.myInterval = 5000;
+
+  setTimeout(function(){
+    $scope.isNjeriConcept = 1;
+  },100);
 
   LandingCarouselService.query(function(data){
     $scope.slides = data;
   });
-
 }]);
