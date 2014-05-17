c62fafee274465b648a4e79aa64beaba7d72c271
Gen Watanabe <gen.watanabe.ca@gmail.com>
2014-05-16 19:37:59 -0700

    Add back-img directive for background image

--- site/app/script-src.js
+++ site/app/script-src.js
@@ -226,3 +226,14 @@ app.factory('GlobalData', function(){
     'defaultTheme': 'spring'
   }
 });
+
+// Background image attribute directive.
+app.directive('backImg', function(){
+    return function(scope, element, attrs){
+        var url = attrs.backImg;
+        element.css({
+            'background-image': 'url(' + url +')',
+            'background-size' : 'cover'
+        });
+    };
+});
