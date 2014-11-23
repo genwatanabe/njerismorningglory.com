15fcb8634bfece402ea1b29855f4bc624cba26d5
Gen Watanabe <happyrice2@gmail.com>
2014-11-22 21:27:54 -0800

    Nursery to Preschool. (reverted from commit 752320d24e463ac6759bf8d31c66c7c5f83f654f)

--- site/app/global/seo.php
+++ site/app/global/seo.php
@@ -1,5 +1,5 @@
 <?php
 	// ADD SEO META TAG HERE
 ?><meta name="Title" content="Waldorf School of the San Jose" />
-<meta name="Description" content="Preschool through Middle School where academics are integrated with creativity, sustainability, and morality" />
-<meta name="Keywords" content="private school, Waldorf school, private school Silicon Valley, School San Jose, Preschool, Kindergarten, Early Childhood, Elementary School, Middle School, Arts, Interdisciplinary Curriculum, San Jose, Silicon Valley education," />
+<meta name="Description" content="Nursery through Middle School where academics are integrated with creativity, sustainability, and morality" />
+<meta name="Keywords" content="private school, Waldorf school, private school Silicon Valley, School San Jose, Nursery, Kindergarten, Early Childhood, Elementary School, Middle School, Arts, Interdisciplinary Curriculum, San Jose, Silicon Valley education," />
